07cd5b1456e841c263d4694fc79ba96fd46e7f43
Lalo (Edward) <67384140+lalohamb@users.noreply.github.com>
2024-12-17 14:37:18 -0600

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,8 +7,8 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
+  const NAME = 'Hood Coin'
+  const SYMBOL = 'HOODc'
   const MAX_SUPPLY = '1000000'
 
   // Deploy Token
